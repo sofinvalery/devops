a2b352dbb1225919404ed6094f1b71d395058058
Valeriy Sofin <valerasofin04@gmail.com>
2026-02-26 16:57:25 +0300

    fix helm deploy conflicts with hpa

--- ops/k8s/deploy-app.sh
+++ ops/k8s/deploy-app.sh
@@ -6,12 +6,19 @@ RELEASE_NAME="${RELEASE_NAME:-reverse}"
 IMAGE_REPOSITORY="${IMAGE_REPOSITORY:-sofinvalery/devops}"
 IMAGE_TAG="${IMAGE_TAG:-latest}"
 CHART_PATH="${CHART_PATH:-./helm/reverse}"
+HELM_FORCE_CONFLICTS="${HELM_FORCE_CONFLICTS:-true}"
 
 kubectl create namespace "${NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
+
+HELM_ARGS=()
+if [ "${HELM_FORCE_CONFLICTS}" = "true" ]; then
+  HELM_ARGS+=(--force-conflicts)
+fi
 
 helm upgrade --install "${RELEASE_NAME}" "${CHART_PATH}" \
   --namespace "${NAMESPACE}" \
   --set image.repository="${IMAGE_REPOSITORY}" \
-  --set image.tag="${IMAGE_TAG}"
+  --set image.tag="${IMAGE_TAG}" \
+  "${HELM_ARGS[@]}"
 
 echo "Application deployed: ${RELEASE_NAME} in namespace ${NAMESPACE}."
